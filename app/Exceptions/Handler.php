3a87431370b4728586eac9604ee5dee7282c6f4e
Billy <billy@monkeytwizzle.com>
2019-08-31 11:50:14 +0100

    Pulled in ignition error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -62,4 +62,13 @@ class Handler extends ExceptionHandler
 
         return redirect()->guest('login');
     }
+
+    protected function whoopsHandler()
+    {
+        try {
+            return app(\Whoops\Handler\HandlerInterface::class);
+        } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+            return (new \Illuminate\Foundation\Exceptions\WhoopsHandler)->forDebug();
+        }
+    }
 }
